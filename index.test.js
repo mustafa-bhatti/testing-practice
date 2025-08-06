a428f365a8eb4fa2f691ed1b785df0adee220e94
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-08-06 20:37:49 +0500

    feat: add test for reverseString

--- index.test.js
+++ index.test.js
@@ -1,4 +1,10 @@
 const capitalize = require('./capitalize');
+const reverseString = require('./reverseString');
+
 test('capitilize the first letter', () => {
   expect(capitalize('hello')).toBe('Hello');
+});
+
+test('reverses the string', () => {
+  expect(reverseString('hello')).toBe('olleh');
 });
